fa720d38a03984474499f1b9fd711dfdd6aae1e9
Dawid Domanski <domanskidawid91@gmail.com>
2020-01-12 16:14:47 +0100

    Break and Continue Lesson 18

--- test.js
+++ test.js
@@ -1,10 +1,16 @@
-var links = document.getElementsByTagName("a")
+for (i = 0; i < 10; i++) {
 
-for (i = 1; i <= links.length; i++) {
+if(i === 5 || i ===3){
+    continue;
+}
+    
+console.log(i);
 
-    console.log("this is link number" + i);
-  
+if(i === 7) {
+
+    break;
+}
 
 }
 
-document.write("all links now looped");
+console.log("I have broken out of the loop")
