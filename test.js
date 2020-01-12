3ecbcb3ff2e82fb36893b99de28d4c0369c97cb8
Dawid Domanski <domanskidawid91@gmail.com>
2020-01-12 15:45:30 +0100

    While Loops Lesson 16

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
-var myAge = 26;
+var age = 4;
 
-if (myAge < 18 || myAge > 30 || myAge ===25) {
-    document.write("You aint coming!");
+while (age < 10) {
+
+    console.log("You age is less than 10");
+    age++;
+
 }
 
-else{
-    document.write("You can come, you cool dude");
-}
+document.write("you are now over 10");
