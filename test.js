789f6ce08270427057a27251c0eb5b2784f2484b
Dawid Domanski <domanskidawid91@gmail.com>
2020-01-14 21:01:33 +0100

    onLoad Event Lesson 41

--- test.js
+++ test.js
@@ -1,16 +1,24 @@
-var content = document.getElementById("content");
-var button = document.getElementById("show-more");
+function setUpEvents(){
+    var content = document.getElementById("content");
+    var button = document.getElementById("show-more");
+    
+    button.onclick = function(){
+    
+        if(content.className == "open"){
+            //shring the bos
+            content.className = "";
+            button.innerHTML = "Show More";
+        } else{
+            //expand box
+            content.className = "open";
+            button.innerHTML = "Show Less";
+        }
+    };
 
-button.onclick = function(){
+}
 
-    if(content.className == "open"){
-        //shring the bos
-        content.className = "";
-        button.innerHTML = "Show More";
-    } else{
-        //expand box
-        content.className = "open";
-        button.innerHTML = "Show Less";
-    }
+window.onload = function(){
 
-};
+    setUpEvents();
+}
+
