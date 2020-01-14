c02ff0c5d84cb6cdcb324e4f94b27b267589e5d0
Dawid Domanski <domanskidawid91@gmail.com>
2020-01-14 13:07:47 +0100

    Constructor Function Lesson 30

--- test.js
+++ test.js
@@ -1,19 +1,22 @@
-var myCar2 = {
+var Car = function(maxSpeed, driver){
 
-    maxSpeed: 70,
-    driver: "Davioli",
-    drive: function(speed, time){
-        console.log(speed * time);
-    },
+    this.maxSpeed = maxSpeed;
+    this.driver = driver;
+    this.drive = function(speed, time){
+        console.log(speed * time);};
+    this.logDriver = function(){
+        console.log("driver name is " + this.driver);
+    };
 
-   logDriver: function(){
-       console.log("driver name is " + this.driver);
-   }
-};
+}
 
-myCar2.logDriver();
-console.log(myCar2.maxSpeed);
-myCar2.drive(50, 3);
+var myCar = new Car(70, "Davioli");
+var myCar2 = new Car(40, "Wiewmag");
+var myCar3 = new Car(10, "Fachura");
+var myCar4 = new Car(90, "DD");
+
+myCar.drive(30,5);
+myCar3.logDriver();
 
 
 
